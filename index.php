92194603b3ad24a9bd46658c2f7f6de44cb4db9a
federicoalagna <federicoalagna01@gmail.com>
2024-06-23 15:25:52 +0200

    [UPD] Style

--- index.php
+++ index.php
@@ -5,12 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista di Dischi</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 <div id="app" class="container">
-    <h1 class="my-4">Lista di Dischi</h1>
+    <h1 class="my-4 text-center">Boolify</h1>
     <div class="row">
         <div class="col-md-4" v-for="disco in dischi" :key="disco.id">
             <div class="card mb-4">
@@ -23,6 +26,8 @@
         </div>
     </div>
 </div>
+
+
     <script>
     new Vue({
         el: '#app',
@@ -40,6 +45,8 @@
         }
     });
 </script>
+
+
 </body>
 </html>
 
